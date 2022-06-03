e14a9628b3a7c64c5c84a3c6313efb40d2470d03
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-03 19:12:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main");
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "JACQUE is the champion";
